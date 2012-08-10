0217eb3d0edd6d242ac8263a8b32d0e6eba2a076
jvitku <devnull@localhost>
2012-08-10 17:38:06 +0200

    zadana prvni chyba

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -31,10 +31,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Ahooj");
-		
-		
-		
+				
 		Logger log = new Logger();		// init logging system
 		Main m = new Main();		
 		
@@ -111,8 +108,9 @@ public class Main {
 		}
 	}
 	
-	public void parse(BufferedReader ntex,BufferedReader temp, 
+	public void parse(BufferedReader ntex, BufferedReader temp, 
 			BufferedWriter wr, Logger log){
+		muhe heeee 
 		
 		Text t = new Text(log);
 		
