65c0e4f8d7a6b75145387116c480acf4d38510b3
jvitku <devnull@localhost>
2012-08-13 15:49:23 +0200

    pridana template, znova..

--- src/main/java/main/TemplateAndHelp.java
+++ src/main/java/main/TemplateAndHelp.java
@@ -24,8 +24,20 @@ public class TemplateAndHelp {
 		
 	}
 	
+	private final String scriptName = "compile";
 	private final String tempName = "notes.ntex";
-	private final String temp1 = "sgfdskghsfd\nkfdsk";
+	private final String temp1 = "\\title{Name of the document}\n\\author{Name of Author}\n"+
+	"\\begin{abstract}\n\tAbstract in one Line..\n\\end{abstract}\n"+
+	"\\section{Section}\nSome normal text..\n-itemized text\n-Some text in \\b{bold} and \\i{italics}.\n"+
+	"\t-higher level..\n\\subsection{Some subsection here}\n-Here is an example of some citation: \\cite{kniha}.\n"+
+	"\n\n-This item is separated by means of several enters from the above one\n"+
+	"-Here we add some figure \\f{a,2,Description of figure}\n"+	
+	"-Here is another reference to already placed figure \\f{a}\n"+
+	"\t-Note that you don't have to specify width or description to the figure \\f{b, something..}.\n" +
+	"\t-Type of figure is determined automatically,currently supported types are:\n" +
+	"\t\t-eps\n\t\t-png\n\t\t-jpg\n"+
+	"\n\n\\begin{literatura}{1}\n\\bibitem{kniha} " +
+	"\n\tAllman J.: \\emph{Evolving Brains}, Scientific American Library Paperback, 2000.\n \\end{literatura}";
 	
 	public void template(){
 		// is there folder for pictures?
@@ -38,11 +50,16 @@ public class TemplateAndHelp {
 		// are there pictures?
 		if(!this.isThere("fig/a.png"))
 			this.createImage("fig/a", 0);
-		if(this.isThere("fig/b.jpg"))
+		if(!this.isThere("fig/b.jpg"))
 			this.createImage("fig/b", 1);
 		
 
 		this.writeTemplate();
+		
+		System.out.println("Template generated in the file: "+tempName+"\n"+
+				"\t-now edit the file '"+tempName+"' and/or\n"+
+				"\t-run the srcipt called '"+scriptName+"' in order to convert " +
+						"in into the LaTeX and generate pdf");
 	}
 	
 	private void writeTemplate(){
@@ -94,9 +111,8 @@ public class TemplateAndHelp {
 	      switch(type){
 	      case 0:
 	    	  ImageIO.write(bi, "PNG", new File(name+app));
-		      
 	      case 1:
-	    	  ImageIO.write(bi, "JPEG", new File(name+app));
+	    	  ImageIO.write(bi, "JPG", new File(name+app));
 		      
 	      }
 	      /*
@@ -104,7 +120,6 @@ public class TemplateAndHelp {
 	      ImageIO.write(bi, "JPEG", new File("c:\\yourImageName.JPG"));
 	      ImageIO.write(bi, "gif", new File("c:\\yourImageName.GIF"));
 	      ImageIO.write(bi, "BMP", new File("c:\\yourImageName.BMP"));
-	      
 	      */
 	    } catch (IOException ie) {
 	      log.err("could not generate image named: "+name+app);
